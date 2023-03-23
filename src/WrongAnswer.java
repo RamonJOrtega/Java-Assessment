de97f6bba2977f88b410f07b591900a2528ebae5
ramon6767 <ramon.justis.ortega@gmail.com>
2023-03-23 12:14:26 -0700

    replace question5.java with WrongAnswer.java

---
+++ src/WrongAnswer.java
@@ -0,0 +1,35 @@
+package src;
+
+public class WrongAnswer {
+
+    private int counter = 0;
+
+    public static void main(String[] args) {
+        new WrongAnswer().run();
+    }
+
+    private void run() {
+        try {
+            Thread t1 = new Thread(this::incrementToOnHundred);
+            Thread t2 = new Thread(this::incrementToOnHundred);
+            t1.start();
+            t2.start();
+            t1.join();
+            t2.join();
+            System.out.println("counter is " + counter);
+        } catch (InterruptedException e) {
+            System.err.println("fatal error, unexpected interrupt exception");
+            System.exit(2);
+        }
+    }
+
+    private void incrementToOnHundred() {
+        for (int i = 0; i < 10_000; i++) {
+            doSomeFakeWork();
+        }
+    }
+
+    private void doSomeFakeWork() {
+        counter++;
+    }
+}
